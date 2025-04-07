d28b87c3e1278a1d1881a219460d69914cfa40e6
Olivia Folsom <86935346+folo1@users.noreply.github.com>
2025-04-07 10:49:50 -0400

    Update main.go

--- docker/main.go
+++ docker/main.go
@@ -10,7 +10,7 @@ type Server struct{}
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
    w.WriteHeader(http.StatusOK)
    w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": "Hello  2025"}`))
+   w.Write([]byte(`{"message": "Test Change for CDCI"}`))
 }
 
 func main() {
